4a2a72180582f3925fe3b351e1a5ade4644101d5
Carl <carlelli5@hotmail.co.uk>
2023-02-09 20:26:40 +0000

    added first html page

--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,5 @@
-from django.shortcuts import render, HttpResponse
-
-# Create your views here.
+from django.shortcuts import render
 
 
-def say_hello(request):
-    return HttpResponse("Hello!")
+def get_todo_list(request):
+    return render(request, 'todo/todo_list.html')
