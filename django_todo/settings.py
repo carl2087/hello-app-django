5f4002543bc5b0ed3ab8ade26df2f6365aa6e9be
Carl <carlelli5@hotmail.co.uk>
2023-02-15 20:52:12 +0000

    updated allowed host

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://hello-app-django.herokuapp.com/']
+ALLOWED_HOSTS = ['hello-app-django.herokuapp.com']
 
 
 # Application definition
